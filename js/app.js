1dd3494749fb79d4c82c20fe5ca0dbb3a151d05d
SarahMalash <sarahabumalash@gmail.com>
2020-02-18 16:38:35 +0200

    gess2

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 'use strict';
+var score = 0
 var yourName = prompt("please enter your name:");
 alert(`HELLO HELLO ${yourName} LET US PLAY A LITTLE GISSING NAME`);
 
@@ -9,6 +10,7 @@ switch (question1.toLowerCase()) {
     case 'y':
         alert("YOU ARE RIGHT!!!");
         console.log("do you think i am a hard worker?", question1);
+        score++
 
         break;
     case 'no':
@@ -30,6 +32,7 @@ switch (question2.toLowerCase()) {
     case 'y':
         alert("YOU ARE RIGHT!!!");
         console.log("do you think i love food?", question2);
+        score++
         break;
     case 'no':
     case 'n':
@@ -48,6 +51,7 @@ switch (question3.toLowerCase()) {
     case 'y':
         alert("YOU ARE RIGHT!!!");
         console.log("do you think i am a family person?", question3);
+        score++
         break;
     case 'no':
     case 'n':
@@ -73,6 +77,7 @@ switch (question4.toLowerCase()) {
 
         alert("YOU ARE RIGHT!!!");
         console.log("do you think i am a pet owner?", question4);
+        score++
         break;
     default:
         alert("Please Try Again, it is a yes or no qusetion ");
@@ -86,6 +91,7 @@ switch (question5.toLowerCase()) {
     case 'y':
         alert("YOU ARE RIGHT!!!");
         console.log("do you think i can drive?", question5);
+        score++
         break;
     case 'no':
     case 'n':
@@ -97,5 +103,52 @@ switch (question5.toLowerCase()) {
         console.log("do you think i can drive?", question5);
         break;
 }
-alert(`THANK YOU ${yourName} I HOPE YOU ENJOYED OUR GAME`);
+
+
+for (var i = 0; i <= 4; i++) {
+    var question6 = prompt("can you guess my age?");
+    if (i == 4) {
+
+        alert("sorry you are have only 4 attempts, the correct answer is 25!");
+        break;
+    }
+
+    if (question6 === 25) {
+        alert("THAT IS CORRECT!! WHAT ARE YOU A spiritual mediator!");
+        console.log("can you guess my age?", question6)
+        score++
+        break;
+    }
+    else if (question6 < 25) {
+
+        alert("you are too low but thank you thoug!! try again");
+        console.log("can you guess my age?", question6)
+    }
+    else if (question6 > 25) {
+        alert("you are too high come on i am not that old!!");
+        console.log("can you guess my age?", question6);
+
+    }
+
+}
+
+
+var carsArray = ["audi", "bmw", "kia", "tesla", "bently", "ford"];
+for (var car = 0; car <= 6; car++) {
+    if (car == 6) {
+        alert("you are out of attempts!! the right answers are: audi, bmw, kia, bently and ford ");
+        break;
+    }
+    //console.log(countryArray[car]);
+    var question7 = prompt("can you guess what is my draem car?");
+    if (question7 === carsArray[car]) {
+        alert("you are right this is one of my dream cars");
+        console.log("can you guess what is my draem car?", question7);
+        score++
+        break;
+
+    }
+}
+alert(`THANK YOU ${yourName} I HOPE YOU ENJOYED OUR GAME AND YOUR SCORE IS ${score} OUT OF 7`);
+
 
